ac4532e1f02a96524d7ecbb9c0344f79ec8a497c
JOLINE29 <jolinefloria@gmail.com>
2026-03-16 14:41:52 +0530

    uc1- welcome message

--- app/src/BookMyShow.java
+++ app/src/BookMyShow.java
@@ -1,9 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class BookMyShow{
+public class BookMyShow {
     public static void main(String[] args) {
         //ore by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("welcome to the hotel booking management system");
-            System.out.println("system initialized succesfully");
-        }
+        System.out.println("welcome to the hotel booking management system");
+        System.out.println("system initialized successfully");
     }
+}
